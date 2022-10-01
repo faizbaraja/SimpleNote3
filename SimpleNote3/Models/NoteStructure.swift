d395aabcb3af35e73b54cd3662f1c86418d5cf4d
faiz baraja <faiz.baraja89@gmail.com>
2022-10-01 11:23:06 +0700

    feat : implement saved mechanisme

---
+++ SimpleNote3/Models/NoteStructure.swift
@@ -0,0 +1,14 @@
+//
+//  NoteStructure.swift
+//  SimpleNote3
+//
+//  Created by faiz baraja on 01/10/22.
+//
+
+import UIKit
+
+struct NoteStructure: Codable {
+    let noteTitle: String?
+    let noteCategory: String?
+    let noteDescription: String?
+}
