56128cd4a68d73a2ef20a033062350e2c4f7dc0e
faiz baraja <faiz.baraja89@gmail.com>
2022-10-01 11:08:03 +0700

    feat : create ui for input notes

---
+++ SimpleNote3/AddNotes/AddNotesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddNotesViewController.swift
+//  SimpleNote3
+//
+//  Created by faiz baraja on 01/10/22.
+//
+
+import UIKit
+
+class AddNotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Add Notes"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ SimpleNote3/AddNotes/AddNotesViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AddNotesViewModel.swift
+//  SimpleNote3
+//
+//  Created by faiz baraja on 01/10/22.
+//
+
+import UIKit
+
+class AddNotesViewModel: NSObject {
+
+}
